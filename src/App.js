9103eca7a2abeffbc928f544e150fa9d3dca5bb5
Fat-Tony <fat_tonyy@icloud.com>
2019-12-26 15:42:06 +0000

    Router-setUp

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React from "react";
 
+// React Router Dom
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// Pages
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProductDetails from "./pages/ProductDetails";
+import Products from "./pages/Products";
+
+// components
+
 export default function App() {
   return <h1>hello from app component</h1>;
 }
