4d24f6f2045c8e63a56fcfc9efccbf340c222d9c
Fat-Tony <fat_tonyy@icloud.com>
2019-12-27 12:12:33 +0000

    About-page-component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Pinterest meditation cronut coloring book, kinfolk selvage plaid
+        brooklyn. Activated charcoal photo booth cronut, ethical raw denim cloud
+        bread yuccie. Shabby chic gluten-free farm-to-table lumbersexual palo
+        santo stumptown freegan. Chartreuse chambray tacos four dollar toast meh
+        street art shoreditch. Four dollar toast swag pok pok, typewriter forage
+        la croix man braid disrupt vape aesthetic roof party fixie taiyaki
+        normcore tofu. Post-ironic actually raclette sartorial, organic cloud
+        bread irony.
+      </p>
+    </section>
+  );
 }
