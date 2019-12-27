3df1122bcf51ab70f9110411b870d5f9b8282fc3
Fat-Tony <fat_tonyy@icloud.com>
2019-12-27 17:11:08 +0000

    product context setup

--- src/context/products.js
+++ src/context/products.js
@@ -1,1 +1,16 @@
 // products context
+import React from "react";
+
+export const ProductContext = React.createContext();
+
+// Provider, consumer
+
+export default function ProductProvider({ children }) {
+  const greeting = "Yo";
+  const product = { id: 1, title: "product name" };
+  return (
+    <ProductContext.Provider value={{ greeting, product }}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
