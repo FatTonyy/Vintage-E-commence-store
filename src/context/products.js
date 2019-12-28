856047fea767f9a45e7eb5f87c529d3e1b02db7a
Fat-Tony <fat_tonyy@icloud.com>
2019-12-28 15:27:28 +0000

    setting values using useState

--- src/context/products.js
+++ src/context/products.js
@@ -1,15 +1,19 @@
 // products context
 import React from "react";
 
+// useState is a method that comes with react and returns two values
+
 export const ProductContext = React.createContext();
 
 // Provider, consumer
 
 export default function ProductProvider({ children }) {
-  const greeting = "Yo";
-  const product = { id: 1, title: "product name" };
+  const [loading, setLoading] = React.useState(false);
+  const [products, setProducts] = React.useState([]);
+  const [featured, setFeatured] = React.useState([]);
+
   return (
-    <ProductContext.Provider value={{ greeting, product }}>
+    <ProductContext.Provider value={{ products, loading, featured }}>
       {children}
     </ProductContext.Provider>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,7 +2,8 @@ import React, { useContext } from "react";
 import { ProductContext } from "../context/products";
 
 export default function Products() {
-  console.log(React.useContext(ProductContext));
+  const response = React.useContext(ProductContext);
+  console.log(response);
 
   return <h1>hello from products page</h1>;
 }
