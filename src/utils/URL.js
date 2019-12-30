0e2b04b416635a5843502e607319017700875cfb
Fat-Tony <fat_tonyy@icloud.com>
2019-12-30 19:03:42 +0000

    strapi instalss

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,1 +1,3 @@
-export default "https://johnsmilgatutorials.com/projects/react-tech-store-v2";
+export default "http://localhost:1337";
+
+//! two fixes for image display [one for local access to images  and second is for when application is  deployed]
