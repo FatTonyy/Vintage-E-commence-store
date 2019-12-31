627f9d1c0b60d019cda8d7c6cacc112d53b9f8d0
Fat-Tony <fat_tonyy@icloud.com>
2019-12-31 16:00:19 +0000

    checkoutPage-component

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -22,5 +22,60 @@ export default function Checkout(props) {
   }
 
   if (cart.length < 1) return <EmptyCart />;
-  return <h1>hello from checkout page</h1>;
+
+  return (
+    <section className="section form">
+      <h2 className="section-title">checkout</h2>
+      <form className="checkout-form">
+        <h3>
+          order total : <span>GHC {total}</span>
+        </h3>
+
+        {/* single input */}
+        <div className="form-control">
+          <label htmlFor="name">name</label>
+          <input
+            type="text"
+            value={name}
+            onchange={e => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        {/* end of single input */}
+
+        {/* card elements */}
+        <div className="stripe-input">
+          <label htmlFor="card-element">credit card or debit card</label>
+          <p className="stripe-info">
+            test using this credit card : <span>4242 4242 4242 4242</span>
+            <br />
+            enter any 5 digits for thr zip code
+            <br />
+            enter any 3 digits for cvc
+          </p>
+        </div>
+        {/* card elements */}
+
+        {/* stripe elements */}
+        {/* end of stripe elements */}
+
+        {/* stripe errors */}
+        {error && <p className="form-empty">{error}</p>}
+
+        {/* empty value */}
+        {isEmpty ? (
+          <p className="form-empty">please fill out name filed</p>
+        ) : (
+          <button
+            type="submit"
+            className="btn btn-primary btn-block"
+            onClick={handleSubmit}
+          >
+            submit
+          </button>
+        )}
+      </form>
+    </section>
+  );
 }
