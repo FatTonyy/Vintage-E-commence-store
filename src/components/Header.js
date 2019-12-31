61d096519eb4aac31c09ed65db95811e3395fac6
Fat-Tony <fat_tonyy@icloud.com>
2019-12-31 15:00:04 +0000

    checout page intro and restrict access

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 import CartLink from "../components/Cart/CartLink";
+import { UserContext } from "../context/user";
+import LoginLink from "../components/LoginLink";
 
 export default function Header() {
+  const { user } = React.useContext(UserContext);
   return (
     <header className="header">
       <img src={logo} alt="Vintage tech logo" className="logo" />
@@ -21,11 +24,17 @@ export default function Header() {
             </li>
           </div>
 
-          <div>
+          {user.token && (
             <li>
-              <Link to="/login">Login</Link>
+              <Link to="/checkout">Checkout</Link>
             </li>
+          )}
+          <div>
+            {/* <li>
+              <Link to="/login">Login</Link>
+            </li> */}
             <CartLink />
+            <LoginLink />
           </div>
         </ul>
       </nav>
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { CartContext } from "../context/cart";
+import { UserContext } from "../context/user";
 import EmptyCart from "../components/Cart/EmptyCart";
 import CartItem from "../components/Cart/CartItem";
 import { Link } from "react-router-dom";
 // import {UserContext}  from '../context/user'
 
 export default function Cart() {
-  let user = false;
   const { cart, total } = React.useContext(CartContext);
+  const { user } = React.useContext(UserContext);
 
   if (cart.length === 0) {
     return <EmptyCart />;
@@ -23,7 +24,7 @@ export default function Cart() {
       <h2>total : GHC {total}</h2>
 
       {/* checking to see if user logs in */}
-      {user ? (
+      {user.token ? (
         <Link to="/checkout" className="btn btn-primary btn-block">
           Checkout
         </Link>
