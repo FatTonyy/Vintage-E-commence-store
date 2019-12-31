4c177dc3bccb206434589c86a11a61fb9d829760
Fat-Tony <fat_tonyy@icloud.com>
2019-12-31 09:48:34 +0000

    login page -setup

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,25 @@
 import React from "react";
 
+// strapi functions
+
+// handle User
+import { useHistory } from "react-router-dom";
+
 export default function Login() {
+  const history = useHistory();
+  // setup user context
+
+  // state values
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [username, setUsername] = React.useState("default");
+  const [isMember, setMember] = React.useState(true);
+
+  let isEmpty = false;
+
+  const toggleMember = () => {};
+
+  const handleSubmit = (async(e) = {});
+
   return <h1>hello from login page</h1>;
 }
