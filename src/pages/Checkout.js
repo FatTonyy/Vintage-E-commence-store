b0c00f223ff6ab3f2d1d58bd8f957a67aa97690f
Fat-Tony <fat_tonyy@icloud.com>
2019-12-31 15:32:35 +0000

    setup-checkout-page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { CartContext } from "../context/cart";
+import { UserContext } from "../context/user";
+import { useHistory } from "react-router-dom";
+import EmptyCart from "../components/Cart/EmptyCart";
 
-export default function Checkout() {
+// react stripe elements
+import SubmitOrder from "../strapi/submitOrder";
+
+export default function Checkout(props) {
+  const { cart, total, clearCart } = React.useContext(CartContext);
+  const { showAlert, user, hideAlert, alert } = React.useContext(UserContext);
+  const history = useHistory();
+
+  // state values
+  const { name, setName } = React.useState("");
+  const { error, setError } = React.useState("");
+  const isEmpty = !name || alert.show;
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  if (cart.length < 1) return <EmptyCart />;
   return <h1>hello from checkout page</h1>;
 }
