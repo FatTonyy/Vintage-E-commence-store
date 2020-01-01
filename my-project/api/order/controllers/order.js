56ed36d06dc67e14ead035fb4736e99bea8f60ef
Fat-Tony <fat_tonyy@icloud.com>
2020-01-01 11:53:03 +0000

    strapi-backend

--- my-project/api/order/controllers/order.js
+++ my-project/api/order/controllers/order.js
@@ -1,8 +1,29 @@
-'use strict';
+"use strict";
+
+const stripe = require("stripe")("sk_test_vw5LeVwa9nQKlmpBnu5x30yj00LCBan5L8");
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  create: async ctx => {
+    const { name, total, items, stripeTokenId } = ctx.request.body;
+    const { id } = ctx.state.user;
+
+    const change = await stripe.charges.create({
+      amount: Math.round(total * 100),
+      currency: "usd", //does not support Ghana cedis yet
+      source: stripeTokenId,
+      description: `order ${new Date()} by ${ctx.state.user.username}`
+    });
+    const order = await strapi.services.order.create({
+      name,
+      total,
+      items,
+      user: id
+    });
+    return order;
+  }
+};
