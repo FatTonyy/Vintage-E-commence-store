3a6e052c020bb1f3c4b11ef3b53cede034186424
Fat-Tony <fat_tonyy@icloud.com>
2020-01-01 13:53:02 +0000

    changed-url-from-local-to-production

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,3 +1,3 @@
-export default "http://localhost:1337";
+export default "https://vintage-store-api.herokuapp.com";
 
 //! two fixes for image display [one for local access to images  and second is for when application is  deployed]
