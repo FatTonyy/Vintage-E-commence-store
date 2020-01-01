b06ae920a8848211e7298ca703ac4ced7451389c
Fat-Tony <fat_tonyy@icloud.com>
2020-01-01 10:53:18 +0000

    submit order setup

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -34,6 +34,26 @@ function Checkout(props) {
 
     const { token } = response;
     if (token) {
+      setError("");
+      const { id } = token;
+      let order = await SubmitOrder({
+        name: name,
+        total: total,
+        items: cart,
+        stripeTokenId: id,
+        userToken: user.token
+      });
+      if (order) {
+        showAlert({ msg: "your order is complete" });
+        clearCart();
+        history.push("/");
+        return;
+      } else {
+        showAlert({
+          msg: "there was an error with your order, please try again!",
+          type: "danger"
+        });
+      }
     } else {
       hideAlert();
       setError(response.error.message);
