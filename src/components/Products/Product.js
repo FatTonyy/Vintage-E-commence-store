8389c74dd915debd2b9d4b96ad1b0cd4326eb337
Fat-Tony <fat_tonyy@icloud.com>
2020-01-17 12:23:45 +0000

    product proptypes

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// initiating prop types //impt is the snippet used
+import PropTypes from "prop-types";
+
+// import default images
+import img from "../../assets/mainBcg.jpg";
+
 export default function Product({ image, title, id, price }) {
   return (
     <article className="product">
       <div className="img-container">
-        <img src={image} alt={title} />
+        <img src={image || img} alt={title || "Default title"} />
         <Link to={`products/${id}`} className="btn btn-primary product-link">
           Details
         </Link>
@@ -13,9 +19,16 @@ export default function Product({ image, title, id, price }) {
 
       {/* footer */}
       <div className="product-footer">
-        <p className="product-title">{title}</p>
-        <p className="product-price">GHC {price}</p>
+        <p className="product-title">{title || "default title"}</p>
+        <p className="product-price">GHC {price || 0}</p>
       </div>
     </article>
   );
 }
+
+Product.propTypes = {
+  image: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired
+};
